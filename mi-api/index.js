26811e3482d6dbd1769d50798e8155d8a10f604c
teran210c <a242729@uach.mx>
2025-03-12 14:32:36 -0600

    get

--- mi-api/index.js
+++ mi-api/index.js
@@ -8,6 +8,24 @@ app.get('/', (req, res)=>{
     res.send('Hola Mundo')
 });
 
+const users = []
+
+app.get('/users', (req,res)=>{
+    res.json(users)
+})
+
+app.get('/users/:id', (req,res)=>{
+    const id = parseInt(req.params.id)
+    const user = users.find(u=>u.id===id);
+    if (user){
+        res.json(user)
+    } else{
+        res.status(404).json({message:"Usuario no encontrado"})
+
+    }
+
+})
+
 app.listen(port, ()=>{
     console.log('Servidor escuchando el puerto', port)
 })
